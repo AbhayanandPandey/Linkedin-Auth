2e07fae47730de2a8252d54b4c2dc54aa85ebc2d
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-05-10 20:54:02 +0530

    updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,9 @@
+
 exports.logoutUser = (req, res) => {
         res.clearCookie('GithubToken', {
             httpOnly: false,
             secure: true, 
             sameSite: 'Lax',
           });
-        res.redirect('http://localhost:5173/');
+        res.redirect(process.env.FRONTEND_URL);
 };
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  const navigate = useNavigate();
+  useNavigate();
 
   const handleLogin = () => {
     window.location.href = 'http://localhost:5001/auth/linkedin';
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-gray-100 to-gray-300">
       <div className="bg-blue-800 shadow-xl rounded-3xl p-10 flex flex-col items-center gap-6 transition-transform duration-300 hover:scale-105">
         <h2 className="text-3xl font-semibold text-white">Welcome Back</h2>
-        <p className="text-white text-center">Sign in with your LinkedIn account</p>
+        <p className="text-white text-center">Sign in with your Linkedin account</p>
         <button
           className="flex items-center gap-3 px-6 py-3 bg-white text-black text-lg rounded-xl shadow-lg transition duration-300 ease-in-out cursor-pointer"
           onClick={handleLogin}
@@ -22,7 +22,7 @@ export default function Home() {
             alt="LinkedIn icon"
             className="w-6 h-6 cursor-pointer"
           />
-          Sign in with LinkedIn
+          Sign in with Linkedin
         </button>
       </div>
     </div>
